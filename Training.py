e0302e90b1307e2ae1e4aa1ab39a01b59b16958b
Owen175 <88042260+Owen175@users.noreply.github.com>
2022-11-30 12:42:41 +0000

    Update Training.py

--- Training.py
+++ Training.py
@@ -8,3 +8,70 @@ from six.moves import urllib
 import tensorflow.compat.v2.feature_column as fc
 
 import tensorflow as tf
+
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+  
+ 
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on testing data
+
+clear_output()
+
+
+
+number = 148
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+result = list(linear_est.predict(eval_input_fn))
+
+
+def did_they_survive():
+  #print(y_eval)
+  dataframe = pd.DataFrame({"sex":["female"],
+                      "age":[72],
+                      "n_siblings_spouses":[0],
+                      "parch":[0],
+                      "fare":[157.0],
+                      "class":["Second"],
+                      "deck":["A"],
+                      "embark_town":["Southampton"],
+                      "alone":["y"]})
+
+  eval_1 = pd.DataFrame({0:[0]})
+  #print(dataframe)
+  eval_input_fn = make_input_fn(dataframe, eval_1, num_epochs=2, shuffle=False)
+  result = list(linear_est.predict(eval_input_fn))
+  print(result[0]['probabilities'][1])
+did_they_survive()
